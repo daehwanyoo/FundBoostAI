0f6769b509d357d13eb7539830946f5c57a06356
daehwanyoo <daehwanyoo0505@gmail.com>
2023-06-15 16:16:56 -0400

    Version 2: Manually select the system device output

--- main.py
+++ main.py
@@ -2,42 +2,46 @@ import speech_recognition as sr
 import openai
 import os
 
-openai.api_key = os.getenv("API KEY")
+for index, name in enumerate(sr.Microphone.list_microphone_names()):
+    print(f'{index}, {name}')
 
-completion=openai.Completion()
+openai.api_key = "YOUR-API"
+
+completion = openai.Completion()
 
 def Reply(donor):
-    response=completion.create(
+    response = completion.create(
         model="gpt-3.5-turbo",
-         messages=[
-        {"role": "system", "content": "You are a helpful assistant to create a script to ultimately persuading potential donors. Bring me one or two or even three sentences to respond or a question for what donor said"},
-        {"role": "user", "content": "{donor}"},
-    ]
-)
-    answer=response.choices[0].message
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant to create a script to ultimately persuading potential donors. Bring me one or two or even three sentences to respond or a question for what donor said"},
+            {"role": "user", "content": f"{donor}"},
+        ]
+    )
+    answer = response.choices[0].message.content
     return answer
 
 def recognize_speech():
     r = sr.Recognizer()
+    mic=sr.Microphone(device_index=1)
     while True:
-        with sr.Microphone() as source:
+        with mic as source:
             r.pause_threshold = 1
             r.adjust_for_ambient_noise(source)
             print("Listening...")
             audio = r.listen(source)
         try:
             print("Recognizing...")
-            query= r.recognize_google(audio,language= 'en-in')
+            query = r.recognize_google(audio, language='en-in')
             words = query.split()
             print(words)
-
+            return query
         except Exception as e:
             print("Couldn't recognize what you said, speak once more.")
 
 if __name__ == '__main__':
     while True:
-        query=recognize_speech().lower()
-        ans=Reply(query)
+        query = recognize_speech().lower()
+        ans = Reply(query)
         print(ans)
         if 'bye' in query:
             break
